9eabf9c77660707a195b98bc1658349745fa1645
ManjunathGanesan <manjunathganesan@gmail.com>
2018-06-24 23:54:33 +0530

    Just Created Simple Employee Class

---
+++ OOPSLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OOPSLibrary
+{
+    public class BaseClass
+    {
+        private int EmployeeID { get; set; }
+        private string EmployeeName { get; set; }
+        private string Location { get; set; }
+    }
+}
